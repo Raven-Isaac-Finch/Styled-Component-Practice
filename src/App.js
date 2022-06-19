cb81c6cadd6de587d09cfa196f6f4f0f6a37b834
Raven I. Finch <purplebox02@gmail.com>
2022-06-19 15:04:38 +0300

    Project Completed

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { ThemeProvider } from 'styled-components';
+
+import { StyledContainer } from './components/styles/Container.styled';
+import Header from './components/Header';
+import Card from './components/Card';
+import Footer from './components/Footer';
+import GlobalStyles from './components/styles/Global';
+import content from './content';
+
+const theme = {
+  colors: {
+    header: '#ebfbff',
+    body: '#fff',
+    footer: '#003333'
+  },
+  mobile: '768px'
+}
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+        <GlobalStyles />
+        <Header />
+        <StyledContainer>
+          {content.map((item, index) => (
+            <Card key={index} item={item}/>
+          ))}
+        </StyledContainer>
+        <Footer />
+      </>
+    </ThemeProvider>
+  );
+}
+
+export default App;
